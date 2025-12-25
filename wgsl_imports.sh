bc2baa663f0f1abf89d887765e53674401699ffd
molikto <molikto@gmail.com>
2025-12-25 10:25:46 +0800

    Revert "bad changes"
    This reverts commit c3af1208ba1bd64c5a4cffd0a9325ae4b622343a.

--- wgsl_imports.sh
+++
@@ -1,1 +0,0 @@
-rg define_import_path -g '*.wgsl' --sort path | sd '^([^:]*):#define_import_path (.*)' ' "$2": "https://raw.githubusercontent.com/bevyengine/bevy/refs/tags/v0.17.1/$1",' >> output.txt
